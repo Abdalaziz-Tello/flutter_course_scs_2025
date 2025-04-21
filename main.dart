44ac741b5c166ecbef00342f5c4ae47f8235a5de
abdtll <abdalaziztll992010@gmail.com>
2025-04-21 18:43:05 +0300

    init ponit

--- main.dart
+++ main.dart
@@ -1,51 +1,36 @@
-main(){
-  // delcration التصريح
-  int counter;
-  // print(counter);
-  // initlaztion الاسناد
-  counter =0;
-  print(counter.runtimeType);
-  // ? int 
-
-  String name;
-  name="Ahmad";
-  print(name.runtimeType);
-  // ? String
-
-  bool isFavorite =true;
-  print(isFavorite.runtimeType);
-  // ? Bool
-  num degree = 88.6;
-  print(degree.runtimeType);
-  degree=200;
-
-  // ! Can't Assigend double to Integar
-  // int poeplation = 20.3;
-
-  // ! Can't Modify the Type of Varaible
-  // var marks=20.4;
-  // marks="Hello";
-  // ? var is KeyWord to Detect the Type
-  var anything=  "Hello Wolrd";
-
-  var suprise;
-  suprise=20;
-  suprise="Hello";
-
-  
-  // marks="Hello";
-  dynamic data="Jkk";
-  data=20;
-  data=true;
-  data = null;
 
 
-  Null empty =null;
-  print(empty);
 
-  int? age=90;
-  print(age.runtimeType);
-  String? nickName;
-  print(nickName);
+void main() {
+  // ? First Solution
+  String message= "Hello World";
+  // print(message);
+  // print(message);
+  // print(message);
 
+  // ? Second Solutaion
+  print(printThreeTimes(message));
+  // ! Make error Because the function is Void
+  // print(sayMyName("Noor"));
+  sayMyName("Noor");
+//  String is_my_name="Hello";
+//  print(is_my_name);
+}
+// ? Singture : must has a type 
+int printThreeTimes(String message){
+  // print(is_my_name);
+  print(message);
+  print(message);
+  print(message);
+// ? body : must Has a return;
+return 20;
+}
+
+
+void sayMyName(String name){
+  int counter = 0;
+  (){
+    print(counter);
+  };
+  print("Hello Mr. "+name);
 }
