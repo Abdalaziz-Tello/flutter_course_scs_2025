3d6f8970524b36daaf35b1b7dd616c28d2793388
abdtll <abdalaziztll992010@gmail.com>
2025-04-26 18:40:28 +0300

    fix bugs

--- main.dart
+++ main.dart
@@ -1,75 +1,29 @@
 
-abstract class User {
-  String name;
-  String jop;
-  int brithDate;
-
-  User({required this.brithDate,required this.jop,required this.name});
-
-  // ? This is an Abstarct Method
-  calculateAge();
-
-  sayMyName(){
-    print("The User name is : "+ name);
-  }
+printButAvoidNull(String name){
+print(name);
 }
 
+void main() async{
+  // print(1==2); // ? Mathmatical Operation
+  // print(1>20);
 
-class Doctor extends User {
-  Doctor({required super.name,required super.brithDate,required super.jop});
+int x = 20;
+//   if (x>30) {
+//     print("object");
+//   }else{
+//     print("not object");
+//   }
+  // ? Inline if
+print(x>30 ? "Object" : "Not Object");
 
-  
+String? nickName;
 
-  @override
-  calculateAge() {
-    print(DateTime.now().year -brithDate + 6);
-  }
-
-  @override
-  sayMyName() {
-    print("The User name is Dr  " + name);
-  }
-}
-
-
-class Engi extends User {
-  Engi({required super.name,required super.brithDate,required super.jop});
-
-  @override
-  calculateAge() {
-    print(DateTime.now().year-brithDate + 20);
-  }
-  // ! If You Did not give the method Behavior 
-  // ! it will take The Father Behavio (USER)
-  // @override
-  // sayMyName() {
-  //   print("The User name is En  " + name);
-  // }
-}
+await Future.delayed(Duration(seconds: 3));
 
 
+print(nickName?? "Hello World");
 
-void main() {
-  // User admin  = User(brithDate: 2002, jop: "Doctor", name: "Ahmad");
-  // admin.sayMyName();
+// ! Note : This will accure an Exception
+// printButAvoidNull(nickName!);
 
-  Doctor doctor = Doctor(name: "Noor", brithDate: 1960, jop: "Doctor");
-  doctor.sayMyName();
-  doctor.calculateAge();
-
-  User testUser = Doctor(name: "Test", brithDate: 0000, jop: "Test");
-  // testUser.sayMyName();
-
-  // tellMeTheTypeOfUser();
-
-}
-
-
-tellMeTheTypeOfUser(User user){
-  if (user.runtimeType == Doctor) {
-    print("This is a Doctor");
-  }else {
-    print("This is An Enginear");
-
-  }
 }
